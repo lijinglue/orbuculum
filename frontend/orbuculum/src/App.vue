5e9a5310bc3d25bc966d0c18b1ac761e5250afc1
lijinglue <lijinglue@live.com>
2017-09-01 01:53:21 +0800

    frontend updates

--- frontend/orbuculum/src/App.vue
+++ frontend/orbuculum/src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="header">
+      <div class="hex-logo">
+        <div class="hex-logo-icon">
+          <img src="./assets/hextech.png" width="45">
+        </div>
+        <div class="hex-logo-text"><span>HEXBET</span></div>
+      </div>
+
+    </div>
+
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+  @import "style/variables.less";
+  @import "style/hextech.less";
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
--- frontend/orbuculum/src/components/Index.vue
+++ frontend/orbuculum/src/components/Index.vue
@@ -1,196 +1,60 @@
 <template>
-<!--MAIN MATCH SHEDULE BEGIN-->
-<div class="main-match-shedule amateurs-match-shedule">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="amateurs-main-match">
-                    <div class="title">Premier League - Round 28</div>
-                    <div class="team">
-                        <div class="avatar"><img src="../images/dota/team-logo1.png" alt="team-logo"></div>
-                        <div class="text">
-                            <span class="name">Internacional</span>
-                            <span class="city">london</span>
-                        </div>
-                    </div>
-                    <div class="score">
-                        3:2
-                    </div>
-                    <div class="team guest">
-                        <div class="text">
-                            <span class="name">Arsenal</span>
-                            <span class="city">Lyon</span>
-                        </div>
-                        <div class="avatar"><img src="../images/dota/team-logo2.png" alt="team-logo"></div>
-                    </div>
-                    <div class="title">26 september / 8:45 PM</div>
-                </div>
-            </div>
-            <div class="col-md-12"><h6>Latest matches</h6></div>
-            <div class="col-md-12">
-                <ul class="tab-filters" role="tablist">
-                    <li class="active"><a href="#nationalcup" role="tab" data-toggle="tab">National cup</a></li>
-                    <li><a href="#championship" role="tab" data-toggle="tab">Championship</a></li>
-                    <li><a href="#friendlies" role="tab" data-toggle="tab">Friendlies</a></li>
-                     <li><a href="#showall" role="tab" data-toggle="tab">Show all</a></li>
-                </ul>
-            </div>
-
-            <div class="tab-content">
-                <div class="tab-pane active" id="nationalcup" role="tabpanel">
-                    <div class="col-md-12">
-                        <div class="main-lates-matches">
-                            <a href="matches.html" class="item">
-                                <span class="championship">National cup - quarterfinal</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo3.png" alt="team-logo"></span><span>Team 1 </span></span>
-                                    <span class="score"><span>3:2</span></span>
-                                    <span class="team1"><span>Team 2 </span><span><img src="../images/dota/team-logo4.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-                            <a href="matches.html" class="item">
-                                <span class="championship">National cup - quarterfinal</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo3.png" alt="team-logo"></span><span>Team 1</span></span>
-                                    <span class="score"><span>3:2</span></span>
-                                    <span class="team1"><span>Team 2</span><span><img src="../images/dota/team-logo4.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane" id="championship" role="tabpanel">
-                    <div class="col-md-12">
-                        <div class="main-lates-matches">
-                            <a href="matches.html" class="item">
-                                <span class="championship">Championship - quarterfinal</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo3.png" alt="team-logo"></span><span>Team 1 </span></span>
-                                    <span class="score"><span>3:2</span></span>
-                                    <span class="team1"><span>Team 2 Internacionasal</span><span><img src="../images/dota/team-logo4.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-                            <a href="matches.html" class="item">
-                                <span class="championship">Championship - quarterfinal</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo3.png" alt="team-logo"></span><span>Team 1</span></span>
-                                    <span class="score"><span>3:2</span></span>
-                                    <span class="team1"><span>Team 2</span><span><img src="../images/dota/team-logo4.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane" id="friendlies" role="tabpanel">
-                    <div class="col-md-12">
-                        <div class="main-lates-matches">
-                            <a href="matches.html" class="item">
-                                <span class="championship">Friendly match</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo3.png" alt="team-logo"></span><span>Team 1</span></span>
-                                    <span class="score"><span>2:2</span></span>
-                                    <span class="team1"><span>Team 2</span><span><img src="../images/dota/team-logo4.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-                            <a href="matches.html" class="item">
-                                <span class="championship">Friendly match</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo1.png" alt="team-logo"></span><span>Team 1</span></span>
-                                    <span class="score"><span>3:3</span></span>
-                                    <span class="team1"><span>Team 2</span><span><img src="../images/dota/team-logo2.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane" id="showall" role="tabpanel">
-                    <div class="col-md-12">
-                        <div class="main-lates-matches">
-                            <a href="matches.html" class="item">
-                                <span class="championship">National cup - quarterfinal</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo3.png" alt="team-logo"></span><span>Team 1 </span></span>
-                                    <span class="score"><span>3:2</span></span>
-                                    <span class="team1"><span>Team 2 </span><span><img src="../images/dota/team-logo4.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-                            <a href="matches.html" class="item">
-                                <span class="championship">National cup - quarterfinal</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo3.png" alt="team-logo"></span><span>Team 1</span></span>
-                                    <span class="score"><span>3:2</span></span>
-                                    <span class="team1"><span>Team 2</span><span><img src="../images/dota/team-logo4.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-                            <a href="matches.html" class="item">
-                                <span class="championship">Championship - quarterfinal</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo3.png" alt="team-logo"></span><span>Team 1 </span></span>
-                                    <span class="score"><span>3:2</span></span>
-                                    <span class="team1"><span>Team 2 Internacionasal</span><span><img src="../images/dota/team-logo4.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-                            <a href="matches.html" class="item">
-                                <span class="championship">Championship - quarterfinal</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo3.png" alt="team-logo"></span><span>Team 1</span></span>
-                                    <span class="score"><span>3:2</span></span>
-                                    <span class="team1"><span>Team 2</span><span><img src="../images/dota/team-logo4.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-                            <a href="matches.html" class="item">
-                                <span class="championship">Friendly match</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo3.png" alt="team-logo"></span><span>Team 1</span></span>
-                                    <span class="score"><span>2:2</span></span>
-                                    <span class="team1"><span>Team 2</span><span><img src="../images/dota/team-logo4.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-                            <a href="matches.html" class="item">
-                                <span class="championship">Friendly match</span>
-                                <span class="teams-wrap">
-                                    <span class="team"><span><img src="../images/dota/team-logo1.png" alt="team-logo"></span><span>Team 1</span></span>
-                                    <span class="score"><span>3:3</span></span>
-                                    <span class="team1"><span>Team 2</span><span><img src="../images/dota/team-logo2.png" alt="team-logo"></span></span>
-                                </span>
-                                <span class="game-result">(5-4) penalties</span>
-                            </a>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="amateurs-main-match">
+    <div class="title">{{prediction}}</div>
+    <a href="#" class="team">
+      <span class="image"><img src="../assets/team-rng.png" alt="main-match"></span>
+      <span class="info">
+                    <span class="name">Team 1</span>
+                    <span class="city">london</span>
+                </span>
+    </a>
+    <div class="score">
+      <span>3:2</span>
     </div>
-</div>
-<!--MAIN MATCH SHEDULE END-->
+    <a href="matches.html" class="team guest">
+                <span class="info">
+                    <span class="name">Team 2</span>
+                    <span class="city">London</span>
+                </span>
+      <span class="image"><img src="../assets/team-rng.png" alt="main-match"></span>
+    </a>
+    <div class="title">26 september / 8:45 PM</div>
+  </div>
+
 </template>
 
 <script>
-export default {
-  name: 'Index',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'Index',
+    data () {
+      return {
+        prediction: 'Which team will be the Summer Champion of LPL?'
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less">
-@scheme-name: "dota";
-@import "../style/less-variables/dota";
-@import "../style/concat";
+  @scheme-name: "dota";
+  @import "../style/less-variables/dota";
+  @import "../style/style";
+  @font_size_large: 24px;
+
+  .amateurs-main-match {
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: url('../assets/up-coming-bg.jpg') no-repeat;
+      z-index: -1;
+      filter: grayscale(0.8) blur(3px);
+    }
+    .title {
+      font-size: @font_size_large;
+    }
+  }
 </style>
